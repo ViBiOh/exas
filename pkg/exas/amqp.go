e8c61a6af4dd7fb3c30fa66930190326e64ef359
Vincent Boutour <bob@vibioh.fr>
2021-12-11 17:30:34 +0100

    refactor: Adding global handler for metrics on err
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/exas/amqp.go
+++ pkg/exas/amqp.go
@@ -17,42 +17,67 @@ type amqpResponse struct {
 	Item model.StorageItem `json:"item"`
 }
 
+var (
+	errNoAccess    = errors.New("exas has no direct access to filesystem")
+	errUnmarshal   = errors.New("unmarshal error")
+	errInvalidPath = errors.New("invalid path")
+	errNotFound    = errors.New("not found")
+	errExtract     = errors.New("extract error")
+	errPublish     = errors.New("publish error")
+)
+
 // AmqpHandler for amqp request
-func (a App) AmqpHandler(message amqp.Delivery) error {
+func (a App) AmqpHandler(message amqp.Delivery) (err error) {
+	defer func() {
+		if err == nil {
+			a.increaseMetric("amqp", "exif", "success")
+			return
+		}
+
+		if errors.Is(err, errNoAccess) {
+			a.increaseMetric("amqp", "exif", "no_access")
+		} else if errors.Is(err, errUnmarshal) {
+			a.increaseMetric("amqp", "exif", "unmarshal_error")
+		} else if errors.Is(err, errInvalidPath) {
+			a.increaseMetric("amqp", "exif", "invalid_path")
+		} else if errors.Is(err, errNotFound) {
+			a.increaseMetric("amqp", "exif", "not_found")
+		} else if errors.Is(err, errExtract) {
+			a.increaseMetric("amqp", "exif", "error")
+		} else if errors.Is(err, errPublish) {
+			a.increaseMetric("amqp", "exif", "publish_error")
+		}
+	}()
+
 	if !a.hasDirectAccess() {
-		return errors.New("exas has no direct access to filesystem")
+		return errNoAccess
 	}
 
 	var item model.StorageItem
-	if err := json.Unmarshal(message.Body, &item); err != nil {
-		a.increaseMetric("amqp", "exif", "invalid")
-		return fmt.Errorf("unable to decode: %s", err)
+	if err = json.Unmarshal(message.Body, &item); err != nil {
+		return fmt.Errorf("unable to decode: %s: %w", err, errUnmarshal)
 	}
 
 	if strings.Contains(item.Pathname, "..") {
-		a.increaseMetric("amqp", "exif", "invalid_path")
-		return errors.New("input path with dots is not allowed")
+		return fmt.Errorf("input path with dots is not allowed: %s", errInvalidPath)
 	}
 
 	inputFilename := filepath.Join(a.workingDir, item.Pathname)
 
-	if info, err := os.Stat(inputFilename); err != nil || info.IsDir() {
-		a.increaseMetric("amqp", "exif", "not_found")
-		return fmt.Errorf("input `%s` doesn't exist or is a directory", item.Pathname)
+	var info os.FileInfo
+	if info, err = os.Stat(inputFilename); err != nil || info.IsDir() {
+		return fmt.Errorf("cannot status input `%s` or is a directory: %s", item.Pathname, errNotFound)
 	}
 
-	exif, err := a.get(inputFilename)
+	var exif model.Exif
+	exif, err = a.get(inputFilename)
 	if err != nil {
-		a.increaseMetric("amqp", "exif", "error")
-		return fmt.Errorf("unable to get exif: %s", err)
+		return fmt.Errorf("unable to get exif: %s: %w", err, errExtract)
 	}
 
-	if err := a.amqpClient.PublishJSON(amqpResponse{Item: item, Exif: exif}, a.amqpExchange, a.amqpRoutingKey); err != nil {
-		a.increaseMetric("amqp", "exif", "publish_error")
-		return fmt.Errorf("unable to publish amqp message: %s", err)
+	if err = a.amqpClient.PublishJSON(amqpResponse{Item: item, Exif: exif}, a.amqpExchange, a.amqpRoutingKey); err != nil {
+		return fmt.Errorf("unable to publish amqp message: %s: %w", err, errPublish)
 	}
-
-	a.increaseMetric("amqp", "exif", "success")
 
 	return nil
 }
