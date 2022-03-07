83e34741b8f66f5439cfcc66faa83caa75ac73d7
Vincent Boutour <bob@vibioh.fr>
2022-03-07 21:21:11 +0100

    feat: Parsing gps date in priority, who have timezone
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/exas/date.go
+++ pkg/exas/date.go
@@ -10,29 +10,19 @@ import (
 
 var (
 	exifDates = []string{
-		"SubSecCreateDate", // this one include timezone
+		"GPSDateTime",
 		"CreateDate",
 		"DateCreated",
 	}
 
-	exifTimezone = "OffsetTime"
-
 	datePatterns = []string{
-		"2006:01:02 15:04:05-07:00",
 		"2006:01:02 15:04:05Z07:00",
-		"2006:01:02 15:04:05MST",
-		"01/02/2006 15:04:05-07:00",
-		"01/02/2006 15:04:05Z07:00",
-		"01/02/2006 15:04:05MST",
-		"1/02/2006 15:04:05-07:00",
-		"1/02/2006 15:04:05Z07:00",
-		"1/02/2006 15:04:05MST",
+		"2006:01:02 15:04:05",
+		"2006:01:02",
 	}
 )
 
 func getDate(exif model.Exif) (time.Time, error) {
-	var dates []string
-
 	for _, exifDate := range exifDates {
 		rawCreateDate, ok := exif.Data[exifDate]
 		if !ok {
@@ -45,24 +35,6 @@ func getDate(exif model.Exif) (time.Time, error) {
 		}
 
 		if createDate, err := parseDate(createDateStr); err == nil {
-			return createDate, nil
-		}
-
-		dates = append(dates, createDateStr)
-	}
-
-	rawTimezone, ok := exif.Data[exifTimezone]
-	if !ok {
-		return time.Time{}, nil
-	}
-
-	timezone, ok := rawTimezone.(string)
-	if !ok {
-		return time.Time{}, fmt.Errorf("key `%s` is not a string", exifTimezone)
-	}
-
-	for _, date := range dates {
-		if createDate, err := parseDate(date + timezone); err == nil {
 			return createDate, nil
 		}
 	}
