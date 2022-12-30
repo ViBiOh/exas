8066e97313cc803e660831b4965d24e1e4db1ab2
Vincent Boutour <bob@vibioh.fr>
2022-12-30 11:37:32 +0100

    fix: Remove description from the exas model
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,37 +2,30 @@ package model
 
 import "time"
 
-// Geocode data
+type Exif struct {
+	Date    time.Time      `json:"date,omitempty"`
+	Data    map[string]any `json:"data,omitempty"`
+	Geocode Geocode        `json:"geocode,omitempty"`
+}
+
+func (e Exif) IsZero() bool {
+	return !e.HasData()
+}
+
+func (e Exif) HasData() bool {
+	return len(e.Data) != 0
+}
+
 type Geocode struct {
 	Address   map[string]string `json:"address,omitempty"`
 	Latitude  float64           `json:"lat,omitempty"`
 	Longitude float64           `json:"lon,omitempty"`
 }
 
-// HasAddress checks if struct has address
 func (g Geocode) HasAddress() bool {
 	return len(g.Address) != 0
 }
 
-// HasCoordinates checks if struct has coordinates
 func (g Geocode) HasCoordinates() bool {
 	return g.Latitude != 0 && g.Longitude != 0
-}
-
-// Exif data
-type Exif struct {
-	Date        time.Time      `json:"date,omitempty"`
-	Data        map[string]any `json:"data,omitempty"`
-	Description string         `json:"description,omitempty"`
-	Geocode     Geocode        `json:"geocode,omitempty"`
-}
-
-// IsZero checks if struct is empty or not
-func (e Exif) IsZero() bool {
-	return !e.HasData() && len(e.Description) == 0
-}
-
-// HasData checks if struct has data and not only description
-func (e Exif) HasData() bool {
-	return len(e.Data) != 0
 }
