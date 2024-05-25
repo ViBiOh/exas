f7dbe17cb3e48cdfdaeea97648919b53aad400ce
Vincent Boutour <bob@vibioh.fr>
2024-05-25 23:08:05 +0200

    fix: Using fast to discard content when good
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/exas/exas.go
+++ pkg/exas/exas.go
@@ -97,7 +97,7 @@ func (s Service) get(ctx context.Context, input io.Reader) (exif model.Exif, err
 	ctx, end := telemetry.StartSpan(ctx, s.tracer, "exiftool")
 	defer end(&err)
 
-	cmd := exec.Command("./exiftool", "-json", "-")
+	cmd := exec.Command("./exiftool", "-json", "-fast", "-")
 
 	buffer := bufferPool.Get().(*bytes.Buffer)
 	defer bufferPool.Put(buffer)
